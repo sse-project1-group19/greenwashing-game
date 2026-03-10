bfa222ad0e2f47edb552d46bd06b3383a13988bf
Conall Lynch <c.lynch43@universityofgalway.ie>
2026-03-10 13:29:37 +0100

    init Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -130,4 +130,13 @@
 
 // export default App;
 
-export { };
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-
+      <App></App>
   </React.StrictMode>,
 );
