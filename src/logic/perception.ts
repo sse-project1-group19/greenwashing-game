cb913ba0e66f2a6e5bd67a7e99cd8c75f6271f96
KingCoenta <coenwerre@gmail.com>
2026-04-01 00:44:48 +0200

    balancing perception degradation to make it less hard to win and more pollution focused

--- src/logic/perception.ts
+++ src/logic/perception.ts
@@ -3,7 +3,7 @@ import { GameState } from "../types/index";
 export const updatePerceptionOnTick = (state: GameState) => {
     var perceptionMax = state.perceptionMax;
     var tick = state.tick;
-    var degradationFactor = map(tick, 0, 10000, 0.001, 0.15); // Example degradation factor
+    var degradationFactor = map(tick, 0, 100000, 0.001, 0.05); // Example degradation factor
     var pollutionImpact = map(state.pollution, 0, 100000, 0, 0.1); // Example pollution impact on perception
     degradationFactor += pollutionImpact; // Increase degradation based on pollution
     var perceptionDegradation = Math.max(0, perceptionMax * degradationFactor);
